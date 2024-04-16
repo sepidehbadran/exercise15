808a8c333164a30e68bbce3eff730620cc4bf3b7
Sepideh <sepideh.badran05@gmail.com>
2024-04-16 18:11:59 +0330

    exersice 15

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            float A, P;
+            const float PI = 3.14f;
+            Console.WriteLine("Enter the Raduis:");
+            r= int .Parse(Console.ReadLine());
+            A = PI * r * r;
+            P = PI * 2 * r;
+            Console.WriteLine("Area is :"+A);
+            Console.WriteLine("Pear is :"+P);
+            Console.ReadLine();
+        }
+    }
+}
